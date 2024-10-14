dc324347e2e6f4181e1e773d7f1eb3b7330f025d
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-10-14 08:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
            
 from streamlit_extras.add_vertical_space import add_vertical_space                  
 from PyPDF2 import PdfReader                             
-from langchain.text_splitter import RecursiveCharacterTextSplitter                                              
+from langchain.text_splitter import RecursiveCharacterTextSplitter                                               
 from langchain.embeddings.openai import OpenAIEmbeddings                           
 from langchain.vectorstores import FAISS                                
                  
