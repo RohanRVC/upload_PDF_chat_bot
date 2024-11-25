306606084fa235770c34129194620b1d42aeefa7
Rohan Vinay Chaudhary <80825254+RohanRVC@users.noreply.github.com>
2024-11-25 14:00:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os   
 import streamlit as st  
 import pickle       
-import google.generativeai as genai             
+import google.generativeai as genai              
                      
 from streamlit_extras.add_vertical_space import add_vertical_space                               
 from PyPDF2 import PdfReader                                       
